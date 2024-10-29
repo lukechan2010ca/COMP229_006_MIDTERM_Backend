52f362a0076d88ee8db6d6b15a273faea8ee3c73
lukechan2010ca <lukechan2010ca@gmail.com>
2024-10-28 23:03:45 -0400

    bug fixed

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "ATLASDB":"mongodb+srv://<username>:<password>@<hostname>/<database>?retryWrites=true&w=majority"
+    "ATLASDB":"mongodb+srv://dbuser:jDDz9cgfl3RaMQP1@clustercomp229.okcdx.mongodb.net/"
+    
 }
